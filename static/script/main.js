b5b1de9b18acf887c56fda2e3cdebf86906a4cce
qincheng <qincheng@unitedstack.com>
2014-12-14 23:44:35 +0800

    update

--- static/script/main.js
+++ static/script/main.js
@@ -8,45 +8,42 @@ var vm = avalon.define({
     userLanguage: browserLanguage() === "zh" ? "zh" : "en",
     currentPath: "zh/introduction.html",
     concepts: [
-            {
-                name: "非监控属性",
-                id: "unobservable"
-            },
-            {
-                name: "监控属性",
-                id: "observable"
-            },
-            {
-                name: "监控数组",
-                id: "collection"
-            },
-            {
-                name: "计算属性",
-                id: "computed"
-            },
-            {
-                name: "$watch方法",
-                id: "$watch"
-            },
-            {
-                name: "$fire方法",
-                id: "$fire"
-            },
-            {
-                name: "视图模型",
-                id: "vmodel"
-            },
-            {
-                name: "数据模型",
-                id: "$model"
-            }],
+        {
+            name: "非监控属性",
+            id: "unobservable"
+        },
+        {
+            name: "监控属性",
+            id: "observable"
+        },
+        {
+            name: "监控数组",
+            id: "collection"
+        },
+        {
+            name: "计算属性",
+            id: "computed"
+        },
+        {
+            name: "$watch方法",
+            id: "$watch"
+        },
+        {
+            name: "$fire方法",
+            id: "$fire"
+        },
+        {
+            name: "视图模型",
+            id: "vmodel"
+        },
+        {
+            name: "数据模型",
+            id: "$model"
+        }],
     changePath: function() {
-        setTimeout(function() {
-            vm.currentPath = location.href.split("#")[1]
-            console.log(vm.currentPath)
-        })
+        vm.currentPath = location.href.split("#")[1]
     },
-    highlight: function(){
-          SyntaxHighlighter.highlight()
+    highlight: function() {
+        SyntaxHighlighter.highlight()
     }
 })
