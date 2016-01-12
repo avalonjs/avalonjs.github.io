ad5bd5a279e741cf31e84dd1c470c3c1e28eebf2
yuhao.ju <heiwu805@hotmail.com>
2016-01-12 15:29:32 +0800

    auto update

--- gulpfile.js
+++ gulpfile.js
@@ -85,8 +85,10 @@ gulp.task('cdn', ['combo'], function(){
 gulp.task('git', ['cdn'], function(){
     gulp.src(['./']).pipe(shell([
       'git add .',
-      "git commit -m 'auto update'"
-    ]))
+      "git commit -m 'auto update'",
+      'git pull cdn master',
+      'git push -u cdn master'
+  ]))
     console.log('git同步成功')
 })
 
